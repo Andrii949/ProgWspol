d01184589136ef4d51da4949b83480e8afd1e413
Andrii949 <andrejdomcuk97@gmail.com>
2026-06-03 13:19:54 +0200

    Stage 3 second part

--- Balls/DataTest/BallUnitTest.cs
+++ Balls/DataTest/BallUnitTest.cs
@@ -20,12 +20,14 @@ namespace ConcurrentProgramming.Data.Test
         {
             Vector initialPosition = new(10.0, 10.0);
             Ball newInstance = new(initialPosition, new Vector(0.0, 0.0), 20.0, 2.0);
+            Vector updatedVelocity = new(1.0, 1.0);
             IVector curentPosition = new Vector(0.0, 0.0);
             int numberOfCallBackCalled = 0;
             newInstance.NewPositionNotification += (sender, position) => { Assert.IsNotNull(sender); curentPosition = position; numberOfCallBackCalled++; };
-            newInstance.Move(new Vector(0.0, 0.0), new Vector(0.0, 0.0));
+            newInstance.Move(new Vector(0.0, 0.0), updatedVelocity);
             Assert.AreEqual<int>(1, numberOfCallBackCalled);
             Assert.AreEqual<IVector>(new Vector(0.0, 0.0), curentPosition);
+            Assert.AreEqual<IVector>(updatedVelocity, newInstance.Velocity);
         }
     }
 }
